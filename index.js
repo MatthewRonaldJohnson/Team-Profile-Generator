82a8b5288d9a7e19a1c23f16962f12194175852a
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-03-23 07:47:18 -0500

    created html file in index, works as intended

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const getNewTeamMemberData = require('./utils/getNewTeamMemberData-function')
 const checkIfDone = require('./utils/checkIfDone-Function')
-const generateHTMLPage = require('./utils/generateHTMLPage-Function')
 const generateHTMLCard = require('./utils/generateHTMLCard-Function')
+const fs = require('fs');
 
-async function init(){
+const starterHTML = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="examplestyle.css">
+    <title>Example</title>
+</head>
+<body>
+    <header class="header">My Team</header>
+    <main class="card-container">
+    `
+
+const endHTML = `    
+</main>
+</body>
+</html>`
+
+async function init() {
+    fs.writeFile('./dist/index.html', starterHTML, (err) => {if (err) { console.error(err) }});
     let moreEnteries = true;
-    const htmlCards = [];
-    while(moreEnteries){
+    while (moreEnteries) {
         const newTeamMember = await getNewTeamMemberData();
         const newTeamMemberCard = generateHTMLCard(newTeamMember)
-        htmlCards.push(newTeamMemberCard);
+        fs.appendFile('./dist/index.html', newTeamMemberCard, (err) => {if (err) { console.error(err) }});
         moreEnteries = await checkIfDone();
     }
-    generateHTMLPage(htmlCards);
+    fs.appendFile('./dist/index.html', endHTML, (err) => {if (err) { console.error(err) }});
 }
 
 init();
+
