9d768373727e2ee760198143e983fb99fdd19103
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-03-22 19:52:42 -0500

    fixed merge

--- utils/getNewTeamMemberData-function.js
+++ utils/getNewTeamMemberData-function.js
@@ -45,43 +45,14 @@ const questions = [
     }
 ]
 
-<<<<<<< HEAD
-async function getNewTeamMemberData() {
-    let output = 'nothing';
-    await inquirer
-        .prompt(genericQuestions)
-=======
-
 async function getNewTeamMemberData() {
     let teamMember;
     await inquirer
         .prompt(questions)
->>>>>>> call-fun-from-fun
         .then((response) => {
             //make response into employee obj
             switch (response.role) {
                 case 'Manager':
-<<<<<<< HEAD
-                    inquirer
-                        .prompt(uniqueQuestions[0])
-                        .then((officeNumber) => {
-                            output= new Manager(response.name, response.id, response.email, officeNumber.officeNumber)
-                        })
-                    break;
-                case 'Engineer':
-                    inquirer
-                        .prompt(uniqueQuestions[0])
-                        .then((github) => {
-                            output = new Engineer(response.name, response.id, response.email, github.github)
-                        })
-                    break;
-                case 'Intern':
-                    inquirer
-                        .prompt(uniqueQuestions[0])
-                        .then((school) => {
-                            output = new Intern(response.name, response.id, response.email, school.school)
-                        })
-=======
                     teamMember = new Manager(response.name, response.id, response.email, response.officeNumber);
                     break;
                 case 'Engineer':
@@ -89,25 +60,11 @@ async function getNewTeamMemberData() {
                     break;
                 case 'Intern':
                     teamMember = new Intern(response.name, response.id, response.email, response.school);
->>>>>>> call-fun-from-fun
                     break;
             }
 
         })
-<<<<<<< HEAD
-    console.log(output + 'logged')
-    return output;
-}
-
-console.log(getNewTeamMemberData());
-
-//module.exports = getNewTeamMemberData;
-
-//how to escape out of the then tree with response data? 
-//should return a Manager, Engineer, or Intern object 
-=======
     return teamMember;
 }
 
 module.exports = getNewTeamMemberData;
->>>>>>> call-fun-from-fun
